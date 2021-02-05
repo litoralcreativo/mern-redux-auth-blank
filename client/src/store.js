1824710ba5a3e5b82f66f3345fe16bf038627b15
gaston <litoralcreatives@gmail.com>
2021-02-04 21:10:12 -0300

    Version 1.0.2

--- client/src/store.js
+++ client/src/store.js
@@ -4,22 +4,17 @@ import rootReducer from './reducers';
 
 const initialState = {};
 
-const middelWare = [thunk];
 
 // const store = createStore(rootReducer, initialState, compose(
 //     applyMiddleware(...middelWare),
 //     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 // ))
 
-if(process.env.NODE_ENV === 'production') {
-    const store = createStore(rootReducer, initialState, compose(
-        applyMiddleware(...middleware)
-    ));
-} else {
-    const store = createStore(rootReducer, initialState, compose(
-        applyMiddleware(...middleware),
-        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-    ));
-}
+const store = process.env.NODE_ENV === 'production'?createStore(rootReducer, initialState, compose(
+    applyMiddleware(...middelWare),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)):createStore(rootReducer, initialState, compose(
+    applyMiddleware(...middleware),
+));
 
 export default store;
